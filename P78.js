5b539028292f8df1e30f05199893346afbaa0dfb
Dhruv373-web <66855605+Dhruv373-web@users.noreply.github.com>
2020-10-31 20:30:21 -0500

    Create P78.js

---
+++ P78.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, Text, Image, TouchableOpacity,TextInput, Alert } from 'react-native';
+import SantaAnimation from '../components/SantaClaus.js';
+import db from '../config';
+import firebase from 'firebase';
+
+export default class WelcomeScreen extends Component {
+  constructor(){
+    super()
+    this.state={
+      emailId : '',
+      password: ''
+    }
+  }
+
+  userLogin = (emailId, password)=>{
+    firebase.auth().signInWithEmailAndPassword(emailId, password)
+    .then(()=>{
+      return Alert.alert("Successfully Login")
+    })
+    .catch((error)=> {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      return Alert.alert(errorMessage)
+    })
+  }
+
+  userSignUp = (emailId, password) =>{
+    firebase.auth().createUserWithEmailAndPassword(emailId, password)
+    .then((response)=>{
+      return Alert.alert("User Added Successfully")
+    })
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      return Alert.alert(errorMessage)
+    });
+  }
